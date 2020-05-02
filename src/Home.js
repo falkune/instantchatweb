327070f4a59b737a395ac26ba43ec935a2514e10
falkune <iboudiokel@gmail.com>
2020-05-02 12:33:22 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -8,63 +8,72 @@ class Home extends React.Component{
 		return(
 			<div id="accueiContainer">
 				<aside id="users">
-						<div class="card-body contacts_body">
-						<ui class="contacts">
-						<li class="active">
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img">
-									<span class="online_icon"></span>
+					<div className="col-md-4 col-xl-3 chat"><div className="card mb-sm-3 mb-md-0 contacts_card">
+					<div className="card-header">
+						<div className="input-group">
+							<input type="text" placeholder="Search..." name="" className="form-control search">
+							<div className="input-group-prepend">
+								<span className="input-group-text search_btn"><i className="fas fa-search"></i></span>
+							</div>
+						</div>
+					</div>
+					<div className="card-body contacts_body">
+						<ui className="contacts">
+						<li className="active">
+							<div className="d-flex bd-highlight">
+								<div className="img_cont">
+									<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" className="rounded-circle user_img">
+									<span className="online_icon"></span>
 								</div>
-								<div class="user_info">
+								<div className="user_info">
 									<span>Khalid</span>
 									<p>Kalid is online</p>
 								</div>
 							</div>
 						</li>
 						<li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg" class="rounded-circle user_img">
-									<span class="online_icon offline"></span>
+							<div className="d-flex bd-highlight">
+								<div className="img_cont">
+									<img src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg" className="rounded-circle user_img">
+									<span className="online_icon offline"></span>
 								</div>
-								<div class="user_info">
+								<div className="user_info">
 									<span>Taherah Big</span>
 									<p>Taherah left 7 mins ago</p>
 								</div>
 							</div>
 						</li>
 						<li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" class="rounded-circle user_img">
-									<span class="online_icon"></span>
+							<div className="d-flex bd-highlight">
+								<div className="img_cont">
+									<img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img">
+									<span className="online_icon"></span>
 								</div>
-								<div class="user_info">
+								<div className="user_info">
 									<span>Sami Rafi</span>
 									<p>Sami is online</p>
 								</div>
 							</div>
 						</li>
 						<li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="http://profilepicturesdp.com/wp-content/uploads/2018/07/sweet-girl-profile-pictures-9.jpg" class="rounded-circle user_img">
-									<span class="online_icon offline"></span>
+							<div className="d-flex bd-highlight">
+								<div className="img_cont">
+									<img src="http://profilepicturesdp.com/wp-content/uploads/2018/07/sweet-girl-profile-pictures-9.jpg" className="rounded-circle user_img">
+									<span className="online_icon offline"></span>
 								</div>
-								<div class="user_info">
+								<div className="user_info">
 									<span>Nargis Hawa</span>
 									<p>Nargis left 30 mins ago</p>
 								</div>
 							</div>
 						</li>
 						<li>
-							<div class="d-flex bd-highlight">
-								<div class="img_cont">
-									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user_img">
-									<span class="online_icon offline"></span>
+							<div className="d-flex bd-highlight">
+								<div className="img_cont">
+									<img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" className="rounded-circle user_img">
+									<span className="online_icon offline"></span>
 								</div>
-								<div class="user_info">
+								<div className="user_info">
 									<span>Rashid Samim</span>
 									<p>Rashid left 50 mins ago</p>
 								</div>
@@ -72,6 +81,8 @@ class Home extends React.Component{
 						</li>
 						</ui>
 					</div>
+					<div className="card-footer"></div>
+				</div>
 				</aside>
 
 				<main id="actualite">
