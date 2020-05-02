dea5203c3b6c355e0a1f1b6ee8e452d11cab0f6d
falkune <iboudiokel@gmail.com>
2020-05-02 17:37:49 +0200

    Collabrotext Commit

--- src/Userlist.js
+++ src/Userlist.js
@@ -7,7 +7,7 @@ class Userlist extends React.Component{
 				<div className="img_cont">
 					<span className="online_icon"></span>
 				</div>
-				{this.key}
+				{this.props.userId}
 				{this.props.userName}
 			</div>
 		)
