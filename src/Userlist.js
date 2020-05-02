18e530cff2740aa824959640c71b02b752f5d8df
falkune <iboudiokel@gmail.com>
2020-05-02 17:03:33 +0200

    Collabrotext Commit

--- src/Userlist.js
+++ src/Userlist.js
@@ -3,17 +3,9 @@ import React from 'react';
 class Userlist extends React.Component{
 	render(){
 		return(
-			<li>
-				<div className="d-flex bd-highlight" id="him">
-					<div className="img_cont">
-						<span className="online_icon"></span>
-					</div>
-					<div className="user_info">
-						<span></span>
-						<p></p>
-					</div>
-				</div>
-			</li>
+			<div className="d-flex bd-highlight" id="him">
+				<span className="online_icon"></span>
+			</div>
 		)
 	}
 }
