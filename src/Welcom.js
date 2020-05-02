fe83894db9c0e06e7efcf5fd122c455086690119
falkune <iboudiokel@gmail.com>
2020-05-02 10:55:59 +0200

    Collabrotext Commit

--- src/Welcom.js
+++ src/Welcom.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 class Welcom extends React.Component{
 
 	render(){
 		return(
-			
+
 		);
 	}
 
