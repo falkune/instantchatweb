9c28238da0aba12995a5a7cf3256aeb057491c38
falkune <iboudiokel@gmail.com>
2020-05-02 12:47:52 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -21,7 +21,7 @@ class Home extends React.Component{
 							<div class="d-flex bd-highlight">
 								<div class="img_cont">
 									<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img">
-									<span class="online_icon"></span>
+									<span class="online_icon"/>
 								</div>
 								<div class="user_info">
 									<span>Khalid</span>
