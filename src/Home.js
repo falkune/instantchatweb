cf5c935cd0220f46b2c29827988ed7bccd9deb2e
falkune <iboudiokel@gmail.com>
2020-05-02 12:39:17 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -10,9 +10,9 @@ class Home extends React.Component{
 				<aside id="users">
 					<div class="card-header">
 						<div class="input-group">
-							<input type="text" placeholder="Search..." name="" class="form-control search">
+							<input type="text" placeholder="Search..." name="" class="form-control search"/>
 							<div class="input-group-prepend">
-								
+								<span class="input-group-text search_btn"><i class="fas fa-search"></i></span>
 							</div>
 						</div>
 					</div>
