be612847d6246af0814d55cc66602628d5b8f45d
falkune <iboudiokel@gmail.com>
2020-05-02 11:15:05 +0200

    Collabrotext Commit

--- src/Registerform.js
+++ src/Registerform.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Accueil from './Accueil';
 
 class Registerform extends React.Component{
 
