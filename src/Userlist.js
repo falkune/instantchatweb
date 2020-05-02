3a60a6ead8ec87b13b912724fcc46fbaa6b94bc8
falkune <iboudiokel@gmail.com>
2020-05-02 12:59:27 +0200

    Collabrotext Commit

---
+++ src/Userlist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Userlist extends React.Component{
+	render(){
+		return(
+			<li class="active">
+							<div class="d-flex bd-highlight">
+								<div class="img_cont">
+									<span class="online_icon"></span>
+								</div>
+								<div class="user_info">
+									<span>Khalid</span>
+									<p>Kalid is online</p>
+								</div>
+							</div>
+						</li>
+		)
+	}
+}
