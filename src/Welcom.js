db2ae5214cd972fe0f6a8596dede5ed02e42648b
falkune <iboudiokel@gmail.com>
2020-05-02 11:10:39 +0200

    Collabrotext Commit

--- src/Welcom.js
+++ src/Welcom.js
@@ -7,7 +7,7 @@ class Welcom extends React.Component{
 	render(){
 		return(
 			<div id="welcomContainer">
-				<Jumbotron fluid>
+				<Jumbotron fluid id="welcom">
 				  <Container>
 				    <h1>Welcom to Instanchat</h1>
 				    <p>
