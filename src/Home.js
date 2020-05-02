2b1405f12e0dd1c00742b45eb22ebc1c30343083
falkune <iboudiokel@gmail.com>
2020-05-03 01:53:33 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -19,9 +19,6 @@ class Home extends React.Component{
 					<div div="searchBar">
 						<div className="input-group">
 							<input type="text" placeholder="Search..." name="" className="form-control search"/>
-							<div className="input-group-prepend">
-								<span className="input-group-text search_btn"><i className="fas fa-search"></i></span>
-							</div>
 						</div>
 					</div>
 				</aside>
