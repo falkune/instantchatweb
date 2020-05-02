83c9388b8bdf5bfd4846564aa26cefe9d77185b0
falkune <iboudiokel@gmail.com>
2020-05-02 23:36:51 +0200

    Collabrotext Commit

--- src/Userlist.js
+++ src/Userlist.js
@@ -4,8 +4,13 @@ class Userlist extends React.Component{
 	render(){
 		return(
 			<div className="d-flex bd-highlight" id="him">
-				<div className="img_cont">
-					<span className="online_icon">{this.props.userName}</span>
+				<div className="him">
+					<div>
+						<span className="online_icon"></span>
+					</div>
+					<div>
+						{this.props.userName}
+					</div>
 				</div>
 			</div>
 		)
