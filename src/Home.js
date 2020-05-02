71988810e13cc1657e8f8acfc664f6a5bd943210
falkune <iboudiokel@gmail.com>
2020-05-02 13:16:56 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -18,17 +18,7 @@ class Home extends React.Component{
 						</div>
 					</div>
 					<div class="card-body contacts_body" id="theUsers">
-						<li>
-				<div class="d-flex bd-highlight" id="him">
-					<div class="img_cont">
-						<span class="online_icon"></span>
-					</div>
-					<div class="user_info">
-						<span>Khalid</span>
-						<p>Kalid is online</p>
-					</div>
-				</div>
-			</li>
+						<Userlist />
 					</div>
 				</aside>
 
