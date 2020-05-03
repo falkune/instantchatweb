955adf1db6a077ad292ef48221bfe0423b41db4e
falkune <iboudiokel@gmail.com>
2020-05-03 21:54:12 +0200

    Collabrotext Commit

--- src/Messagebox.js
+++ src/Messagebox.js
@@ -3,7 +3,22 @@ import React from 'react';
 class Messagebox extends React.Component{
 
 	render(){
-		
+		return (
+			<Modal.Dialog>
+			  <Modal.Header closeButton>
+			    <Modal.Title>Modal title</Modal.Title>
+			  </Modal.Header>
+
+			  <Modal.Body>
+			    <p>Modal body text goes here.</p>
+			  </Modal.Body>
+
+			  <Modal.Footer>
+			    <Button variant="secondary">Close</Button>
+			    <Button variant="primary">Save changes</Button>
+			  </Modal.Footer>
+			</Modal.Dialog>
+		)
 	}
-	
+
 }
