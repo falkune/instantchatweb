4d572b4d69aa09c6e181ebf663a35bee0cfe992b
falkune <iboudiokel@gmail.com>
2020-05-03 02:42:49 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ class Home extends React.Component{
 					<div className="card-body contacts_body" id="userBox">
 						{
 							this.props.users.map(user => 
-								<Userlist key={user['user_id'].toString()} userId={user['user_id']} userName={user['user_name']} onClick={this.showMessage()}/>
+								<Userlist key={user['user_id'].toString()} userId={user['user_id']} userName={user['user_name']} onClick={this.showMessage}/>
 							)
 						}
 					</div>
