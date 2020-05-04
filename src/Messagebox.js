d411d883565478e3872ba7bd03283fde7cb5e948
falkune <iboudiokel@gmail.com>
2020-05-04 03:08:25 +0200

    Collabrotext Commit

--- src/Messagebox.js
+++ src/Messagebox.js
@@ -60,7 +60,7 @@ class Messagebox extends React.Component{
 			return(
 				<div id="emojibox">
 					{
-				    this.state.discution.map(msg => <div>helo worl!</div>)
+				    this.state.discution.map(msg => <div>{msg['message']}</div>)
 				  }
 			  </div>
 			)
