6028a28b30238b9e9f2df54cc5937d6df69f78eb
falkune <iboudiokel@gmail.com>
2020-05-04 23:52:59 +0200

    Collabrotext Commit

--- src/Messagebox.js
+++ src/Messagebox.js
@@ -55,7 +55,6 @@ class Messagebox extends React.Component{
 	}
 
 	showMessages =() => {
-		console.log(this.getDiscution);
 		return(
 			<Modal.Body id="modal-body">
 			{
@@ -72,6 +71,7 @@ class Messagebox extends React.Component{
 	}
 
 	render(){
+		console.log(this.props.connectedUser+' '+this.props.interlocutor);
 
 		if(this.getDiscution === []){
 			return(
