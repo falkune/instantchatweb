2a18cc141ac5d7e60bb16d8f056dafcc6e84b07d
falkune <iboudiokel@gmail.com>
2020-05-04 04:18:00 +0200

    Collabrotext Commit

--- src/Messagebox.js
+++ src/Messagebox.js
@@ -3,6 +3,7 @@ import Modal from 'react-bootstrap/Modal';
 import ModalDialog from 'react-bootstrap/ModalDialog';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Singlemessage './Singlemessage';
 
 class Messagebox extends React.Component{
 
