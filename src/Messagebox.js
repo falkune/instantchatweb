f1f278909f23e9aa7dc0b9f7533e24ba5ab82f7d
falkune <iboudiokel@gmail.com>
2020-05-04 04:17:08 +0200

    Collabrotext Commit

--- src/Messagebox.js
+++ src/Messagebox.js
@@ -63,7 +63,7 @@ class Messagebox extends React.Component{
 				    this.state.discution.map(msg => 
 				    	<div 
 				    		user={msg['transmitter']}
-				    		message={message['message']}
+				    		content={message['message']}
 				    	/>
 				    )
 				  }
