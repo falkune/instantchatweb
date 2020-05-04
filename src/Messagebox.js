f9e9824eb044a46373f4a7502544f213bc57e814
falkune <iboudiokel@gmail.com>
2020-05-04 23:34:25 +0200

    Collabrotext Commit

--- src/Messagebox.js
+++ src/Messagebox.js
@@ -11,13 +11,13 @@ class Messagebox extends React.Component{
 		message : ''
 	}
 
-	componentDidUpdate(){
-		this.getDiscution();
-	}
+	// componentDidUpdate(){
+	// 	this.getDiscution();
+	// }
 
-	componentDidUpdate(){
-		this.getDiscution();
-	}
+	// componentDidUpdate(){
+	// 	this.getDiscution();
+	// }
 
 	getDiscution = () => {
 		const url = 'http://instantchat.com/api/show/'+this.props.connectedUser+'/'+this.props.interlocutor+'/'+this.props.token;
@@ -55,53 +55,82 @@ class Messagebox extends React.Component{
 	}
 
 	showMessages = () => {
-		if(this.state.discution === 0)
-			return <Modal.Body id="modal-body"/>
-		else{
-			return(
-				<Modal.Body id="modal-body">
-					{
-				    this.state.data.map(msg => 
-				    	<Singlemessage
-				    		connectedUser={this.props.connectedUser}
-				    		user={msg['transmitter']}
-				    		content={msg['message']}
-				    	/>
-				    )
-				  }
-			  </Modal.Body>
-			)
-		}
+		return(
+			<Modal.Body id="modal-body">
+			{
+				this.state.data.map(msg => 
+					<Singlemessage
+					connectedUser={this.props.connectedUser}
+					user={msg['transmitter']}
+					content={msg['message']}
+					/>
+					)
+			}
+			</Modal.Body>
+		)
 	}
 
 	render(){
-		return (
-			<Modal.Dialog id="modal-dialog">
-			  <Modal.Header  id="modal-header" closeButton/>
-			  	{this.showMessages()}
-			  <Modal.Footer id="modal-footer">
-			    <Form id="messagetype" onSubmit={this.handleSubmit}>
-						<textarea 
-							id="form-control" 
-							class="form-control type_msg" 
-							placeholder="Type your message..."
-							onChange={this.handleMessage}
-						/>
-						<div class="input-group-append">
-							<span class="input-group-text send_btn">
-								<Button 
-									variant="primary" 
-									type="submit" 
-									id="envoyer"
-									>
-									<i class="fas fa-location-arrow"></i>
-								</Button>
-							</span>
-						</div>
-			    </Form>
-			  </Modal.Footer>
-			</Modal.Dialog>
-		)
+
+		if(this.getDiscution.length === 0){
+			return(
+				<Modal.Dialog id="modal-dialog">
+				  <Modal.Header  id="modal-header" closeButton/>
+				  
+					<Modal.Body id="modal-body"></Modal.Body>
+				  
+				  <Modal.Footer id="modal-footer">
+				    <Form id="messagetype" onSubmit={this.handleSubmit}>
+							<textarea 
+								id="form-control" 
+								class="form-control type_msg" 
+								placeholder="Type your message..."
+								onChange={this.handleMessage}
+							/>
+							<div class="input-group-append">
+								<span class="input-group-text send_btn">
+									<Button 
+										variant="primary" 
+										type="submit" 
+										id="envoyer"
+										>
+										<i class="fas fa-location-arrow"></i>
+									</Button>
+								</span>
+							</div>
+				    </Form>
+				  </Modal.Footer>
+				</Modal.Dialog>
+			)
+		}else{
+			return (
+				<Modal.Dialog id="modal-dialog">
+				  <Modal.Header  id="modal-header" closeButton/>
+				  
+				  <Modal.Footer id="modal-footer">
+				    <Form id="messagetype" onSubmit={this.handleSubmit}>
+							<textarea 
+								id="form-control" 
+								class="form-control type_msg" 
+								placeholder="Type your message..."
+								onChange={this.handleMessage}
+							/>
+							<div class="input-group-append">
+								<span class="input-group-text send_btn">
+									<Button 
+										variant="primary" 
+										type="submit" 
+										id="envoyer"
+										>
+										<i class="fas fa-location-arrow"></i>
+									</Button>
+								</span>
+							</div>
+				    </Form>
+				  </Modal.Footer>
+				</Modal.Dialog>
+			)
+		}
 	}
 
 }
