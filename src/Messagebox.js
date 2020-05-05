101166f26c19ac603ff3d0087109b9025f004c05
falkune <iboudiokel@gmail.com>
2020-05-05 23:45:40 +0200

    Collabrotext Commit

--- src/Messagebox.js
+++ src/Messagebox.js
@@ -15,8 +15,6 @@ class Messagebox extends React.Component{
 		return (
 			<Modal.Dialog id="modal-dialog">
 			  <Modal.Header  id="modal-header" closeButton/>
-
-			  {this.showMessages()}
 			  
 			  <Modal.Footer id="modal-footer">
 			    <Form id="messagetype" onSubmit={this.sendMessage}>
