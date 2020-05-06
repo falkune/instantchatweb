329f93f5866971cee839f51d7ae2165a8f445497
falkune <iboudiokel@gmail.com>
2020-05-07 01:50:43 +0200

    Collabrotext Commit

--- src/Pubbox.js
+++ src/Pubbox.js
@@ -4,7 +4,12 @@ class Pubbox extends React.Component{
 
 	render(){
 		return(
-			<div id="pubox"/>
+			<div id="pubox">
+				<p>
+					hello je viens de creer mon site avec WIX, je vous montre?
+					Dabord rendez vous sur wix.com et puis bla bla..................................................................................................................................................................................................aaaaaaaaaaaaaaaaaaaaaaa
+				</p>
+			</div>
 		)
 	}
 
