98f062e6cacd261581eafd50e9533e9f8c00c8a0
falkune <iboudiokel@gmail.com>
2020-05-07 23:17:54 +0200

    Collabrotext Commit

--- src/Actumeteo.js
+++ src/Actumeteo.js
@@ -4,7 +4,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 class Actumeteo extends React.Component{	
 
 	render(){
-		console.log(this.props.meteo.city_info.name)
+		console.log(this.props.meteo.city_info)
 		return(
 			<Jumbotron id="jumbotron">
 				<h1></h1>
