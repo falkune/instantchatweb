605607d120d262bd7b3254a27a191c7aa50e5b1e
falkune <iboudiokel@gmail.com>
2020-05-07 02:29:12 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -75,7 +75,16 @@ class Home extends React.Component{
 
 				<main id="actualite">
 
-					
+					<Jumbotron>
+					  <h1>Hello, world!</h1>
+					  <p>
+					    This is a simple hero unit, a simple jumbotron-style component for calling
+					    extra attention to featured content or information.
+					  </p>
+					  <p>
+					    <Button variant="primary">Learn more</Button>
+					  </p>
+					</Jumbotron>
 
 					<div id="boxmessage"></div>
 					
