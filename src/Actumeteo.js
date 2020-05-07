77ff2bda18b690ffad4ff3da4b1a40ec0d227931
falkune <iboudiokel@gmail.com>
2020-05-07 16:01:07 +0200

    Collabrotext Commit

--- src/Actumeteo.js
+++ src/Actumeteo.js
@@ -4,7 +4,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 class Actumeteo extends React.Component{	
 
 	render(){
-		
+		console.log()
 		return(
 			<Jumbotron id="jumbotron">
 				<h1></h1>
