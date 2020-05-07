97072db46bdca77286ae1a7a1c434be1b9858ef0
falkune <iboudiokel@gmail.com>
2020-05-07 14:14:45 +0200

    Collabrotext Commit

--- src/Actumeteo.js
+++ src/Actumeteo.js
@@ -26,8 +26,7 @@ class Actumeteo extends React.Component{
 			<Jumbotron id="jumbotron">
 				<h1>Hello, world!</h1>
 				<p>
-					This is a simple hero unit, a simple jumbotron-style component for calling
-					extra attention to featured content or information.
+					{this.getMeteo.ICON}
 				</p>
 				<p>
 
