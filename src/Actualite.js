d18a2af82e52c23d82fa5c1072c9d83355c05a56
falkune <iboudiokel@gmail.com>
2020-05-08 00:37:41 +0200

    Collabrotext Commit

--- src/Actualite.js
+++ src/Actualite.js
@@ -6,24 +6,7 @@ class Actualite extends React.Component{
 	render(){
 		return(
 			<Jumbotron id="jumbotron">
-				<h1>{this.props.meteo.city_info.name}</h1>
-				<div id="journee">
-					<div>
-						<h5>sunrise</h5>
-						<span>{this.props.meteo.city_info.sunrise}</span>
-					</div>
-						
-					<div>
-						<h5>sunset</h5>
-						<span>{this.props.meteo.city_info.sunset}</span>
-					</div>
-				</div>
-				<div>
-					<div>
-						<img src={this.props.meteo.current_condition.icon} alt=""/>
-					</div>
-					<strong>{this.props.meteo.current_condition.tmp} °C</strong>
-				</div>
+				
 			</Jumbotron>
 		)
 	}
