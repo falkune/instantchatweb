197d153dab64f6a8594e86e2f53d85e245925f90
falkune <iboudiokel@gmail.com>
2020-05-07 14:11:40 +0200

    Collabrotext Commit

--- src/Actumeteo.js
+++ src/Actumeteo.js
@@ -22,7 +22,16 @@ class Actumeteo extends React.Component{
 
 	render(){
 		return(
+			<Jumbotron id="jumbotron">
+				<h1>Hello, world!</h1>
+				<p>
+					This is a simple hero unit, a simple jumbotron-style component for calling
+					extra attention to featured content or information.
+				</p>
+				<p>
 
+				</p>
+			</Jumbotron>
 		)
 	}
 }
