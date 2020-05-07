f23b5792bd1373cf76675ec21905d7b1655678b2
falkune <iboudiokel@gmail.com>
2020-05-07 02:01:33 +0200

    Collabrotext Commit

--- src/Loginform.js
+++ src/Loginform.js
@@ -60,10 +60,18 @@ class Loginform extends React.Component{
         })
       }
       else{
-        const TEXT = document.createElement("span");
-        let content = document.createTextNode('This email is no exist get regisred...');
-        TEXT.appendChild(content);
-        document.getElementById('passwordError').appendChild(TEXT);
+        if(data.message == "the email is not correct"){
+          const TEXT = document.createElement("span");
+          let content = document.createTextNode('This email is no exist get regisred...');
+          TEXT.appendChild(content);
+          document.getElementById('passwordError').appendChild(TEXT);
+        }
+        else{
+          const TEXT = document.createElement("span");
+          let content = document.createTextNode('the passwor is not correct...');
+          TEXT.appendChild(content);
+          document.getElementById('passwordError').appendChild(TEXT);
+        }
       }
     });
   }
