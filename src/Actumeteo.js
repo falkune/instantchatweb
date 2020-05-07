a08d2668fdc4ca0cb21ed423aebec086ff49b827
falkune <iboudiokel@gmail.com>
2020-05-07 15:38:34 +0200

    Collabrotext Commit

--- src/Actumeteo.js
+++ src/Actumeteo.js
@@ -3,36 +3,14 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class Actumeteo extends React.Component{
 
-	state = {
-		meteo : "helo"
-	}
-
-	componentDidMount(){
-		this.getMeteo();
-	}
-
-	getMeteo(){
-		const url = 'https://www.prevision-meteo.ch/services/json/paris';
-		this.fetchData(url)
-		.then(data => {
-			this.setState({
-				meteo : data
-			});
-		})
-	}
-
-	fetchData(url){
-    return fetch(url)
-      .then((response) => response.json())
-      .catch((error) => console.error(error))
-  }
+	
 
 	render(){
 		console.log(this.state.meteo);
 		return(
 			<Jumbotron id="jumbotron">
 				<p>
-					<h1>{this.state.meteo.city_info.name}</h1>
+					<h1></h1>
 				</p>
 				<p>
 
