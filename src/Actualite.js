842647b5120df8975f439b636fe8a449250f8b3b
falkune <iboudiokel@gmail.com>
2020-05-08 00:44:54 +0200

    Collabrotext Commit

--- src/Actualite.js
+++ src/Actualite.js
@@ -14,6 +14,15 @@ class Actualite extends React.Component{
 			  <p>
 			   
 			  </p>
+
+			  <h1>Hello, world!</h1>
+			  <p>
+			    This is a simple hero unit, a simple jumbotron-style component for calling
+			    extra attention to featured content or information.
+			  </p>
+			  <p>
+			   
+			  </p>
 			</Jumbotron>
 		)
 	}
