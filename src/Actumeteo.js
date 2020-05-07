acdb03993b98c70a667aadb456ff7a0f6c54080e
falkune <iboudiokel@gmail.com>
2020-05-07 14:05:38 +0200

    Collabrotext Commit

--- src/Actumeteo.js
+++ src/Actumeteo.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Actumeteo extends React.Component{
+
+	ComponentDidMount(){
+
+	}
+
+	getMeteo(){
+		
+	}
+
+	fetchData(url){
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+  }
+
 	render(){
 		return(
 
