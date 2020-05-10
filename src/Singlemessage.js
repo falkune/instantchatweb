a36c9944554eb4b5136794f2d6a1137010d5f609
falkune <iboudiokel@gmail.com>
2020-05-10 15:00:01 +0200

    Collabrotext Commit

--- src/Singlemessage.js
+++ src/Singlemessage.js
@@ -12,7 +12,7 @@ class Singlemessage extends React.Component{
 		let hour = date.getHours();
 		let min = date.getMinutes();
 
-		return date.toLocaleDateString(undefined, options);
+		return date.toLocaleDateString(undefined, options)+'à'+hour+':'+min;
 	}
 
 	render(){
