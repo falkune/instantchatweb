ffaf262e8c959376de3276632272fd04a3c80e49
falkune <iboudiokel@gmail.com>
2020-05-13 01:41:35 +0200

    Collabrotext Commit

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class Login extends React.Component{
 	render(){
+		<Form className="alert" id="loginform" >
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label className="btn-toolbar alert-link">Email address :</Form.Label>
+          <Form.Control 
+            type="email" 
+            placeholder="Enter email"
+            // onChange={this.handleEmail}
+          />
+        </Form.Group>
 
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label className="btn-toolbar alert-link">Password :</Form.Label>
+          <Form.Control 
+            type="password" 
+            placeholder="Password"
+            // onChange={this.handlePassword}
+          />
+        </Form.Group>
+
+        <div id="passwordError">
+          
+        </div>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+      </Form>
 	}
 }
 
