1673916d9bd0f4904288600a1638a95ab988efee
falkune <iboudiokel@gmail.com>
2020-05-13 00:51:21 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 
-function App(){
-  
-  return (
-    <div className="App">
-      <Header/>
-      <div id="container">
-      	
-      </div>
-    </div>
-  );
-  
+class App extends React.Component{
+
+	state = {
+		
+	}
+
+	render(){
+
+	}
+
 }
 
 
