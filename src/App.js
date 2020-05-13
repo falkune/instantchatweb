0cf044d14d66d311b260743c2f98986d13a48bdf
falkune <iboudiokel@gmail.com>
2020-05-13 22:28:25 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -25,34 +25,34 @@ class App extends React.Component{
 		});
 	}
 
-	getConnected = (login, password) => {
-		const URL = 'http://instantchat.com/Api/login/'+login+'/'+password;
-		this.fetchData(URL)
-		.then(data => {
-			if(data.status === 'ok' || data.status === 'active'){
-				this.setState({
-					connectedUser : data.id,
-					token : data.token,
-					name : data.name,
-					page : 'accueil'
-				});
-			}
-			else{
-        if(data.message === "the email is not correct"){
-          const TEXT = document.createElement("span");
-          let content = document.createTextNode('This email is no exist get regisred...');
-          TEXT.appendChild(content);
-          document.getElementById('passwordError').appendChild(TEXT);
-        }
-        else{
-          const TEXT = document.createElement("span");
-          let content = document.createTextNode('the passwor is not correct...');
-          TEXT.appendChild(content);
-          document.getElementById('passwordError').appendChild(TEXT);
-        }
-      }
-		});
-	}
+	// getConnected = (login, password) => {
+	// 	const URL = 'http://instantchat.com/Api/login/'+login+'/'+password;
+	// 	this.fetchData(URL)
+	// 	.then(data => {
+	// 		if(data.status === 'ok' || data.status === 'active'){
+	// 			this.setState({
+	// 				connectedUser : data.id,
+	// 				token : data.token,
+	// 				name : data.name,
+	// 				page : 'accueil'
+	// 			});
+	// 		}
+	// 		else{
+ //        if(data.message === "the email is not correct"){
+ //          const TEXT = document.createElement("span");
+ //          let content = document.createTextNode('This email is no exist get regisred...');
+ //          TEXT.appendChild(content);
+ //          document.getElementById('passwordError').appendChild(TEXT);
+ //        }
+ //        else{
+ //          const TEXT = document.createElement("span");
+ //          let content = document.createTextNode('the passwor is not correct...');
+ //          TEXT.appendChild(content);
+ //          document.getElementById('passwordError').appendChild(TEXT);
+ //        }
+ //      }
+	// 	});
+	// }
 
 	fetchData(url){
     return fetch(url)
