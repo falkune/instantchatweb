7ba90df68e6ea64d8bf34a4b6b17c550f0459457
falkune <iboudiokel@gmail.com>
2020-05-13 03:30:11 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,22 @@ import React from 'react';
 class Home extends React.Component{
 	render(){
 		return(
-			<div className="col-md-4 col-xl-3 chat">
-				<div className="card mb-sm-3 mb-md-0 contacts_card">
-					<div className="card-header">
+			<div className="row justify-content-center h-100">
+				<div className="col-md-4 col-xl-3 chat">
+					<div className="card mb-sm-3 mb-md-0 contacts_card">
+						<div className="card-header">
 
-					</div>
+						</div>
 
-					<div className="card-body contacts_body">
+						<div className="card-body contacts_body">
 
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className="col-md-8 col-xl-6 chat">
-				
+				<div className="col-md-8 col-xl-6 chat">
+
+				</div>
 			</div>
 		)
 	}
