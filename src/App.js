6a534aa9aae5690e21d9a4367335d1e8b79022a0
falkune <iboudiokel@gmail.com>
2020-05-13 22:29:01 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from './Logo';
 import Login from './Login';
 import Register from './Register';
 import Home from './Home';
+import getConnected from './Function';
 
 class App extends React.Component{
 
