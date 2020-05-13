b7e0883d6efef1025ba6f419022d1d70184aa1f8
falkune <iboudiokel@gmail.com>
2020-05-14 00:46:52 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -30,7 +30,7 @@ class Home extends React.Component{
 						<div className="card-header">
 							<div className="d-flex bd-highlight">
 								<div className="user_info">
-									<span id="info_user"></span>
+									<span id="info_user">{this.props.name}</span>
 
 									<p id="nb_messages"></p>
 								</div>
