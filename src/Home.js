d5dd87a7bc5233c9d8c9ec2472fb935b632bd041
falkune <iboudiokel@gmail.com>
2020-05-13 03:22:20 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,9 @@ class Home extends React.Component{
 	render(){
 		return(
 			<div className="col-md-4 col-xl-3 chat">
-				
+				<div className="card mb-sm-3 mb-md-0 contacts_card">
+					
+				</div>
 			</div>
 		)
 	}
