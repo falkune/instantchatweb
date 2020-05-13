e1ee1317fa13b980ccb741822da73ea1000a492d
falkune <iboudiokel@gmail.com>
2020-05-13 16:42:26 +0200

    Collabrotext Commit

--- src/Textarea.js
+++ src/Textarea.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class Textarea extends React.Component{
 	render(){
 		return(
-			
+			<Form id="messagetype" onSubmit={this.handleSubmit}>
+				<textarea 
+					id="form-control" 
+					className="form-control type_msg" 
+					placeholder="Type your message..."
+					onChange={this.handleMessage}
+				/>
+				<span className="input-group-text" id="send_btn">
+					<Button type="submit" id="btn">
+						<i className="fas fa-location-arrow"></i>
+					</Button>
+					
+				</span>
+	    </Form>
 		)
 	}
 }
