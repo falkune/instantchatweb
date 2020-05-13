3fe0436f9cd301d1f86b04fa0bfe9ba5e98fc888
falkune <iboudiokel@gmail.com>
2020-05-13 15:24:40 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component{
 	render(){
 		return(
 			<div className="row">
-				<div className="col-md-4 col-xl-3 chat">
+				<div className="chat">
 					<div className="card mb-sm-3 mb-md-0 contacts_card">
 						<div className="card-header">
 							<Input/>
