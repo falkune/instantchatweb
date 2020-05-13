0b03d7d5d807c4728d2a60c24db5dcf892ed0fa1
falkune <iboudiokel@gmail.com>
2020-05-14 01:58:19 +0200

    Collabrotext Commit

--- src/User.js
+++ src/User.js
@@ -5,9 +5,6 @@ class User extends React.Component{
 		return(
 			<div className="d-flex bd-highlight" id="him">
 				<div className="img_cont">
-					<div id="statut">
-						<span className="online_icon"></span>
-					</div>
 					<div id="name">
 						{this.props.userName}
 					</div>
