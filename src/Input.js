12548ef9b6f09e55dd96e22559691ec49dc9a223
falkune <iboudiokel@gmail.com>
2020-05-13 16:28:31 +0200

    Collabrotext Commit

--- src/Input.js
+++ src/Input.js
@@ -3,13 +3,13 @@ import React from 'react';
 class Input extends React.Component{
 	render(){
 		return(
-			<div className="input-group">
+			<div id="input-group" className="input-group">
 				<div className="input-group-prepend">
 					<div id="the_users">
 						<i className="fas fa-users fa-2x"></i>
 					</div>
 
-					<div id="input-group" className="input-group">
+					<div className="input-group">
 						<input 
 							type="text" 
 							placeholder="Search..." 
