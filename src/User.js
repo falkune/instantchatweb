31559bd1db0d4e0ec5c7dc253aaca6308e00fd92
falkune <iboudiokel@gmail.com>
2020-05-14 16:39:01 +0200

    Collabrotext Commit

--- src/User.js
+++ src/User.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import fetchData from './Function';
-import Messagebox from './Messagebox';
+import Singlemessage from './Singlemessage';
+import Singlemessage from './Singlemessage';
+// import Messagebox from './Messagebox';
 
 class User extends React.Component{
 
