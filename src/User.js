510ac70b30011032014d4e0542a3fc53647b2709
falkune <iboudiokel@gmail.com>
2020-05-14 16:16:08 +0200

    Collabrotext Commit

--- src/User.js
+++ src/User.js
@@ -16,7 +16,7 @@ class User extends React.Component{
 				this.setState({
 					messages : data.data
 				});
-				console.log(data.data);
+				console.log(this.state.messages);
 			}
 			else{
 				console.log('so bad!')
