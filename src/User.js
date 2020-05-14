d8049e749b3b2697bb83c6b30f54199f31fee1ff
falkune <iboudiokel@gmail.com>
2020-05-14 15:18:19 +0200

    Collabrotext Commit

--- src/User.js
+++ src/User.js
@@ -3,14 +3,14 @@ import React from 'react';
 class User extends React.Component{
 
 	getdiscution = () => {
-
+		this.props.discustion
 	}
-	
+
 	render(){
 		return(
 			<div className="d-flex bd-highlight" id="him">
 				<div className="img_cont">
-					<div id="name" onClick={this.props.discustion}>
+					<div id="name" onClick={this.getdiscution}>
 						{this.props.userName}
 					</div>
 				</div>
