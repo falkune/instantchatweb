d4e29ca3d244703682a3903147e24e627d4373c3
falkune <iboudiokel@gmail.com>
2020-05-15 14:20:19 +0200

    Collabrotext Commit

--- src/Home.js
+++ src/Home.js
@@ -51,11 +51,11 @@ class Home extends React.Component{
 						</div>
 
 						<div className="card-body" id="fil-discution">
-							{
+							
 								if(this.state.messages.length === 0){
-									
+
 								}
-							}
+							
 						</div>
 
 						<div className="card-footer">
