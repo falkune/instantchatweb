0257b3c35e47e11470eb7af956c7e7ff37ddb086
falkune <iboudiokel@gmail.com>
2020-05-15 23:38:53 +0200

    Collabrotext Commit

--- src/Textarea.js
+++ src/Textarea.js
@@ -28,7 +28,7 @@ class Textarea extends React.Component{
 			});
 		}
 		else{
-			
+
 		}
 	}
 
@@ -50,7 +50,7 @@ class Textarea extends React.Component{
 
 				<div className="input-group-append" onClick={this.handleSubmit}>
 					<span className="input-group-text send_btn">
-						<i className="fas fa-location-arrow"></i>
+						<a href=""><i className="fas fa-location-arrow"></i></a>
 					</span>
 				</div>
 			</div>
