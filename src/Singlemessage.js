eeff4e42046c799d14b79b82ea1de98c0f9cb742
falkune <iboudiokel@gmail.com>
2020-05-15 16:03:58 +0200

    Collabrotext Commit

--- src/Singlemessage.js
+++ src/Singlemessage.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 class Singlemessage extends React.Component{
 
+	parsedate = (date) => {
+		date = new Date(date*1000);
+		const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+		let hour = date.getHours();
+		let min = date.getMinutes();
+
+		return date.toLocaleDateString(undefined, options)+' à '+hour+':'+min;
+	}
+
 	render(){
 		if(this.props.user === this.props.connectedUser){
 			return(
@@ -29,15 +38,6 @@ class Singlemessage extends React.Component{
 				</div>
 			)
 		}
-	}
-
-	parsedate = (date) => {
-		date = new Date(date*1000);
-		const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-		let hour = date.getHours();
-		let min = date.getMinutes();
-
-		return date.toLocaleDateString(undefined, options)+' à '+hour+':'+min;
 	}
 
 }
