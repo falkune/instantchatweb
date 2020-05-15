389db91921106026ceb85e9c77fed5402aa23b18
falkune <iboudiokel@gmail.com>
2020-05-15 21:55:54 +0200

    Collabrotext Commit

--- src/Input.js
+++ src/Input.js
@@ -3,22 +3,18 @@ import React from 'react';
 class Input extends React.Component{
 	render(){
 		return(
-			<div id="input-group" className="input-group">
+			<div className="input-group">
+				<input 
+					type="text" 
+					placeholder="Search..." 
+					className="form-control search"
+				/>
 				<div className="input-group-prepend">
-					<div className="input-group">
-						<input 
-							type="text" 
-							placeholder="Search..." 
-							className="form-control search"
-						/>
-						<div className="input-group-prepend">
-							<span className="input-group-text search_btn">
-								<i className="fas fa-search"></i>
-							</span>
-						</div>
-					</div>
+					<span className="input-group-text search_btn">
+						<i className="fas fa-search"></i>
+					</span>
 				</div>
-			</div>
+			</div>	
 		)
 	}
 }
