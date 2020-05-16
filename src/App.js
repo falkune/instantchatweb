85438908e0a2a37753053e97f1e28d6d0213e41c
falkune <iboudiokel@gmail.com>
2020-05-17 00:24:19 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends React.Component{
 	state = {
 		page : 'login',
 		connectedUser : undefined,
-		connectedUsers : undefined,
     token : undefined,
     users : undefined,
     name : undefined
