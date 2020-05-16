7949657b1f73869a8e3266b53c2b4bfbb3dd519e
falkune <iboudiokel@gmail.com>
2020-05-16 22:55:33 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -74,10 +74,6 @@ class App extends React.Component{
 		})
 	}
 
-	getConnectedUser = () => {
-		
-	}
-
   getRegistred = (name, email, password) => {
   	const URL = 'http://instantchat.com/Api/register/'+name+'/'+email+'/'+password;
     fetchData(URL)
