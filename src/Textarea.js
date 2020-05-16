41ffefd9ff794a1816ff14442a53557a7fb4ba25
falkune <iboudiokel@gmail.com>
2020-05-16 04:04:24 +0200

    Collabrotext Commit

--- src/Textarea.js
+++ src/Textarea.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import fetchData from './Function';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+// import Button from 'react-bootstrap/Button';
 
 class Textarea extends React.Component{
 
