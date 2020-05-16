0925f066ab60764d1c388b85bd2f0a2157473bb4
falkune <iboudiokel@gmail.com>
2020-05-16 23:50:49 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends React.Component{
 					users : data.data,
 					page : 'accueil'
 				});
-				console.log(this.state.connectedUser)
+				console.log(this.state.connectedUsers)
 			}
 		})
 	}
