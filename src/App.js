81b9ca05b98913309c91f53b98f4e489dc4dbc9c
falkune <iboudiokel@gmail.com>
2020-05-17 01:28:45 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -64,13 +64,14 @@ class App extends React.Component{
 		fetchData(URL)
 		.then(data => {
 			if(data.status === 'ok'){
+				this.getConnectedUser(id, token);
+
 				this.setState({
 					connectedUser : id,
 					token : token,
 					name : name,
 					users : data.data,
-					page : 'accueil',
-					connectedUsers : this.getConnectedUser(id, token)
+					page : 'accueil'
 				});
 			}
 		})
@@ -81,8 +82,9 @@ class App extends React.Component{
 		fetchData(URL)
 		.then(data => {
 			if(data.status === 'ok'){
-				console.log(data.data)
-				return data.data;
+				this.setState({
+					connectedUsers : data.data
+				});
 			}
 		})
 	}
