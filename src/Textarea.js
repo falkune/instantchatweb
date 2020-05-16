538bba7299a761dc9644f4c32bb1b936c63a7ca3
falkune <iboudiokel@gmail.com>
2020-05-16 04:11:07 +0200

    Collabrotext Commit

--- src/Textarea.js
+++ src/Textarea.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import fetchData from './Function';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
