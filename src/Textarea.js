aa66ce1ebbe01dfa60af3b6e1b48dd5fd461e7ed
falkune <iboudiokel@gmail.com>
2020-05-16 16:09:40 +0200

    Collabrotext Commit

--- src/Textarea.js
+++ src/Textarea.js
@@ -36,7 +36,7 @@ class Textarea extends React.Component{
 	}
 
 	useThisEmoji = (emo) => {
-		document.getElementById('saisie').value(this.state.message+' '+emo);
+		document.getElementById('saisie').focus().innerHTML = this.state.message+' '+emo;
 	}
 
 	showemojis = () => {
