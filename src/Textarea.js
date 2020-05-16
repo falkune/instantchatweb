571abf71dcd1dd8e174e153d0ed12609f51184a5
falkune <iboudiokel@gmail.com>
2020-05-16 04:10:12 +0200

    Collabrotext Commit

--- src/Textarea.js
+++ src/Textarea.js
@@ -36,7 +36,23 @@ class Textarea extends React.Component{
 	}
 
 	showemojis = () => {
-		console.log('emojis')
+		ReactDOM.render(
+			<Modal.Dialog>
+			  <Modal.Header closeButton>
+			    <Modal.Title>Modal title</Modal.Title>
+			  </Modal.Header>
+
+			  <Modal.Body>
+			    <p>Modal body text goes here.</p>
+			  </Modal.Body>
+
+			  <Modal.Footer>
+			    <Button variant="secondary">Close</Button>
+			    <Button variant="primary">Save changes</Button>
+			  </Modal.Footer>
+			</Modal.Dialog>,
+			document.getElementById('fil-discution');
+		)
 	}
 
 	render(){
