fc78cdbcf1ca0d8c6708b4b2d3e6b98ce6a850af
falkune <iboudiokel@gmail.com>
2020-05-16 23:03:57 +0200

    Collabrotext Commit

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ class App extends React.Component{
 			if(data.status === 'ok'){
 				this.setState({
 					connectedUser : id,
+					connectedUsers : getConnectedUser,
 					token : token,
 					name : name,
 					users : data.data,
