5620ccee90ae66fb7f0003c703de3537ce5329ef
falkune <iboudiokel@gmail.com>
2020-05-16 03:10:44 +0200

    Collabrotext Commit

--- src/Textarea.js
+++ src/Textarea.js
@@ -29,7 +29,7 @@ class Textarea extends React.Component{
 			});
 		}
 		else{
-			document.getElementById('saisie').value = 'je ne sais pas à qui l\'envoyer' ;
+			document.getElementById('saisie').value = 'je ne sais pas à qui l\'envoyer';
 		}
 	}
 
