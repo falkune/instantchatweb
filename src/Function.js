7bdfb6da993798c3912d4729563e42a689b6a9f3
falkune <iboudiokel@gmail.com>
2020-05-16 23:11:26 +0200

    Collabrotext Commit

--- src/Function.js
+++ src/Function.js
@@ -6,6 +6,7 @@ export default function fetchData(url){
 
 export function getConnectedUser(id, token){
 	const URL = 'http://instantchat.com/Api/Users/'+id+'/'+token;
+	fetchData(URL)
 	.then(data => {
 		if(data.status === 'ok'){
 			return data.data
