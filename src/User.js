19d8d61e794b0c114fba220da1f0ba1dd43ab680
falkune <iboudiokel@gmail.com>
2020-05-17 14:41:25 +0200

    Collabrotext Commit

--- src/User.js
+++ src/User.js
@@ -49,6 +49,7 @@ class User extends React.Component{
 	}
 
 	render(){
+		console.log(this.props.online+ ' '+ this.props.user)
 		if(this.props.online.find(element => element === this.props.user)){
 			return(
 				<div className="d-flex bd-highlight" id="him" onClick={this.getDiscution}>
