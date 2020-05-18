81428a87b626560a435ebd46eda34437a4ad57eb
falkune <iboudiokel@gmail.com>
2020-05-18 02:45:16 +0200

    Collabrotext Commit

--- src/User.js
+++ src/User.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import fetchData from './Function';
-import Singlemessage from './Singlemessage';
+// import Singlemessage from './Singlemessage';
 
 class User extends React.Component{
 
-	state = {
-		messages : undefined
-	}
+	// state = {
+	// 	messages : undefined
+	// }
 
 	getDiscution = () =>{
 		const URL = 'http://instantchat.com/api/show/'+this.props.connectedUser+'/'+this.props.user+'/'+this.props.token;
@@ -15,38 +15,38 @@ class User extends React.Component{
 		fetchData(URL)
 		.then(data => {
 			if(data.status === 'ok'){
-				this.setState({
-					messages : data.data
-				});
+				// this.setState({
+				// 	messages : data.data
+				// });
 				
-				this.props.setIntelocutor(this.props.user, this.props.userName);
+				this.props.setIntelocutor(this.props.user, this.props.userName, data.data);
 
-				if(this.state.messages.length !== 0){
-					ReactDOM.render(
-						<div id="modal-body">
-							{
-								this.state.messages.map(msg =>
-									<Singlemessage
-										key={msg['message_id']}
-										connectedUser={this.props.connectedUser}
-										user={msg['from_user_id']}
-										content={msg['message_body']}
-										editdate={msg['message_edit_at']}
-									/>
-								)
-							}
-						</div>,
-						document.getElementById('fil-discution')
-					);
-				}
-				else{
-					ReactDOM.render(
-						<div/>,document.getElementById('fil-discution')
-					);
-				}
+				// if(this.state.messages.length !== 0){
+				// 	ReactDOM.render(
+				// 		<div id="modal-body">
+				// 			{
+				// 				this.state.messages.map(msg =>
+				// 					<Singlemessage
+				// 						key={msg['message_id']}
+				// 						connectedUser={this.props.connectedUser}
+				// 						user={msg['from_user_id']}
+				// 						content={msg['message_body']}
+				// 						editdate={msg['message_edit_at']}
+				// 					/>
+				// 				)
+				// 			}
+				// 		</div>,
+				// 		document.getElementById('fil-discution')
+				// 	);
+				// }
+				// else{
+				// 	ReactDOM.render(
+				// 		<div/>,document.getElementById('fil-discution')
+				// 	);
+				// }
 			}
 		})
-		setTimeout(this.getDiscution, 2000);
+		// setTimeout(this.getDiscution, 2000);
 	} 
 
 
